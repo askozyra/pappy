7d467e3c5ec4b0680fc5c3e670e5341af1e40ef2
askozyra <khnu2019@gmail.com>
2025-11-15 14:35:05 +0300

    Configured default window.

--- Core/App.xaml.cs
+++ Core/App.xaml.cs
@@ -9,7 +9,18 @@
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            return new Window(new AppShell());
+            return new Window(new AppShell())
+            {
+                Title = AppInfo.Current.Name,
+#if WINDOWS
+                TitleBar = new TitleBar()
+                {
+                    Icon = "appicon.ico",
+                    Title = AppInfo.Current.Name,
+                    BackgroundColor = new Color(0.2f) // #333
+                }
+#endif
+            };
         }
     }
 }
